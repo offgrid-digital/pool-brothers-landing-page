272c2f1afa5e5e2abbb3446803dc2182f86bd609
unknown <aquib_j@yahoo.com>
2023-04-20 11:19:41 +0530

    Bug Fixes

--- asset/main.js
+++ asset/main.js
@@ -19,3 +19,63 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+
+// const date = new Date();
+
+// let day = date.getDate();
+// let current_month = date.getMonth() + 1;
+// let current_year = date.getFullYear();
+
+// // This arrangement can be altered based on how we want the date's format to appear.
+
+
+
+
+function setNextDate() {
+    // Get current date
+    var date = new Date();
+
+    // Add five days to current date
+    date.setDate(date.getDate() + 5);
+
+
+    let day = date.getDate();
+    let current_month = date.getMonth() + 1;
+    let current_year = date.getFullYear();
+
+    let currentDate = `${day}-${current_month}-${current_year}`;
+
+    $("#next-date").text(currentDate)
+}
+
+function setCurrentMonth() {
+
+    // Creating a date object
+    var today = new Date();
+
+    // Getting full month name (e.g. "June")
+    var month = today.toLocaleString('default', { month: 'long' });
+
+    // e.preventDefault();
+
+    $("#current-month").text(month)
+}
+
+
+$(document).ready(function () {
+    
+
+    setCurrentMonth();
+    setNextDate();
+
+
+});
+
+
+
+
+
+
